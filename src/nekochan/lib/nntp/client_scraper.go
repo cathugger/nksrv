46180f6ee30a5bbe0825492020a5ebb23c83d061
cathugger <cathugger@cock.li>
2018-11-25 14:42:15 +0000

    split up scraper code a bit

--- src/nekochan/lib/nntp/client_scraper.go
+++ src/nekochan/lib/nntp/client_scraper.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"net"
 	tp "net/textproto"
 	"time"
@@ -30,115 +31,6 @@ func NewNNTPScraper(db ScraperDatabase, logx LoggerX) *NNTPScraper {
 	c := &NNTPScraper{db: db}
 	c.log = NewLogToX(logx, fmt.Sprintf("nntpscraper.%p", c))
 	return c
-}
-
-func (c *NNTPScraper) readDotLine(dr *bufreader.DotReader) ([]byte, error) {
-	i := 0
-	for {
-		b, e := dr.ReadByte()
-		if e != nil {
-			return c.inbuf[:i], e
-		}
-		if b == '\n' {
-			return c.inbuf[:i], nil
-		}
-		if i >= len(c.inbuf) {
-			return c.inbuf[:i], errTooLargeResponse
-		}
-		c.inbuf[i] = b
-		i++
-	}
-}
-
-func (c *NNTPScraper) readOnlyNewsgroup(
-	dr *bufreader.DotReader) ([]byte, error) {
-
-	i := 0
-	end := 0
-	for {
-		b, e := dr.ReadByte()
-		if e != nil {
-			return c.inbuf[:end], e
-		}
-		if b == '\n' {
-			if end == 0 {
-				end = i
-			}
-			if end == 0 || !FullValidGroupSlice(c.inbuf[:end]) {
-				return nil, fmt.Errorf("bad group %q", c.inbuf[:end])
-			}
-			return c.inbuf[:end], nil
-		}
-		if end == 0 {
-			if b == ' ' || b == '\t' {
-				end = i
-				continue
-			}
-			if i >= len(c.inbuf) {
-				return nil, errTooLargeResponse
-			}
-			c.inbuf[i] = b
-			i++
-		}
-	}
-}
-
-func parseListActiveLine(
-	line []byte) (name []byte, hiwm, lowm uint64, status []byte, err error) {
-
-	i := 0
-	skipWS := func() {
-		for i < len(line) && (line[i] == ' ' || line[i] == '\t') {
-			i++
-		}
-	}
-	skipNonWS := func() {
-		for i < len(line) && line[i] != ' ' && line[i] != '\t' {
-			i++
-		}
-	}
-
-	//skipWS()
-	s := i
-	skipNonWS()
-	if s >= i || !FullValidGroupSlice(line[s:i]) {
-		err = fmt.Errorf("bad group %q", line[s:i])
-		return
-	}
-	name = line[s:i]
-
-	skipWS()
-	s = i
-	skipNonWS()
-	if s >= i || !isNumberSlice(line[s:i]) {
-		err = fmt.Errorf("bad hiwm %q", line[s:i])
-		return
-	}
-	hiwm = stoi64(line[s:i])
-
-	skipWS()
-	s = i
-	skipNonWS()
-	if s >= i || !isNumberSlice(line[s:i]) {
-		err = fmt.Errorf("bad lowm %q", line[s:i])
-		return
-	}
-	lowm = stoi64(line[s:i])
-
-	skipWS()
-	s = i
-	skipNonWS()
-	// can be empty I guess... I don't see why not
-	status = line[s:i]
-
-	// treat any extra as error
-	skipWS()
-	if i < len(line) {
-		err = fmt.Errorf("unknown extra data: %q", line[i:])
-		return
-	}
-
-	return
 }
 
 func (c *NNTPScraper) doActiveList() (err error, fatal bool) {
@@ -203,6 +95,10 @@ func (c *NNTPScraper) doActiveList() (err error, fatal bool) {
 			} else {
 				hiwm = 0
 			}
+		}
+		// safeguard
+		if int64(hiwm) < 0 {
+			hiwm = math.MaxInt64
 		}
 		old_id, e := c.db.GetGroupID(gname)
 		if e != nil {
@@ -398,26 +294,19 @@ func (c *NNTPScraper) doGroup(
 
 	if code == 211 {
 
-		c.args, _ = parseResponseArguments(rest, 4, c.args[:0])
-		if len(c.args) < 3 ||
-			!isNumberSlice(c.args[0]) ||
-			!isNumberSlice(c.args[1]) ||
-			!isNumberSlice(c.args[2]) {
-
-			return -1, fmt.Errorf(
-				"bad successful group response %q",
-				au.TrimWSBytes(rest)), false, false
+		var num, lo, hi uint64
+		num, lo, hi, err = c.parseGroupResponse(rest)
+		if err != nil {
+			return
 		}
-
-		num := stoi64(c.args[0])
-		lo := stoi64(c.args[1])
-		hi := stoi64(c.args[2])
-
-		c.args = c.args[:0]
 
 		if lo > hi || num == 0 {
 			// empty group
 			hi = 0
+		}
+		// safeguard
+		if int64(hi) < 0 {
+			hi = math.MaxInt64
 		}
 		new_id = int64(hi) // we need only high id
 		return
@@ -429,108 +318,6 @@ func (c *NNTPScraper) doGroup(
 			"bad GROUP err %d %q",
 			code, au.TrimWSBytes(rest)), false, false
 	}
-}
-
-func (c *NNTPScraper) getOverLineInfo(
-	dr *bufreader.DotReader) (
-	id uint64, msgid FullMsgID, err error) {
-
-	i := 0
-	nomore := false
-	eatField := func() (field []byte, err error) {
-		s := i
-		for {
-			b, e := dr.ReadByte()
-			if e != nil {
-				err = e
-				return
-			}
-			if b == '\n' {
-				field = c.inbuf[s:i]
-				nomore = true
-				return
-			}
-			if b == '\t' {
-				field = c.inbuf[s:i]
-				return
-			}
-			if i >= len(c.inbuf) {
-				err = errTooLargeResponse
-				return
-			}
-			c.inbuf[i] = b
-			i++
-		}
-	}
-	ignoreField := func() (err error) {
-		for {
-			b, e := dr.ReadByte()
-			if e != nil {
-				err = e
-				return
-			}
-			if b == '\n' {
-				nomore = true
-				return
-			}
-			if b == '\t' {
-				return
-			}
-		}
-	}
-
-	defer func() {
-		if !nomore {
-			for {
-				b, e := dr.ReadByte()
-				if e != nil || b == '\n' {
-					return
-				}
-			}
-		}
-	}()
-
-	// {RFC 2980}
-	// (article number goes before these, ofc)
-	// The sequence of fields must be in this order:
-	// subject, author, date, message-id, references,
-	// byte count, and line count.
-
-	// number
-	snum, err := eatField()
-	if err != nil || nomore {
-		return
-	}
-	snum = au.TrimWSBytes(snum)
-	if len(snum) == 0 || !isNumberSlice(snum) {
-		err = fmt.Errorf("bad id %q", snum)
-		return
-	}
-	id = stoi64(snum)
-	// subject, author, date
-	for xx := 0; xx < 3; xx++ {
-		err = ignoreField()
-		if err != nil {
-			return
-		}
-		if nomore {
-			err = errors.New("wanted more fields")
-			return
-		}
-	}
-	// message-id
-	smsgid, err := eatField()
-	if err != nil {
-		return
-	}
-	smsgid = au.TrimWSBytes(smsgid)
-	msgid = FullMsgID(smsgid)
-	if !ValidMessageID(msgid) {
-		err = fmt.Errorf("invalid msg-id %q", smsgid)
-		return
-	}
-
-	return
 }
 
 func (c *NNTPScraper) eatArticle(msgid FullMsgIDStr) (err error, fatal bool) {
@@ -641,62 +428,6 @@ func (c *NNTPScraper) processTODOList(
 	return
 }
 
-func (c *NNTPScraper) eatHdrLine(
-	dr *bufreader.DotReader) (
-	id uint64, msgid FullMsgID, err error) {
-
-	line, err := c.readDotLine(dr)
-	if err != nil {
-		return
-	}
-
-	i := 0
-	skipWS := func() {
-		for i < len(line) && (i == ' ' || i == '\t') {
-			i++
-		}
-	}
-	skipNonWS := func() {
-		for i < len(line) && i != ' ' && i != '\t' {
-			i++
-		}
-	}
-
-	skipWS()
-	s := i
-	skipNonWS()
-	if s >= i {
-		// empty line
-		return
-	}
-	snum := line[s:i]
-	// {RFC 2980}
-	if au.EqualFoldString(unsafeBytesToStr(snum), "(none)") {
-		return
-	}
-	if !isNumberSlice(snum) {
-		err = fmt.Errorf("bad id %q", snum)
-		return
-	}
-	id = stoi64(snum)
-
-	skipWS()
-	s = i
-	skipNonWS()
-	msgid = FullMsgID(line[s:i])
-	if !ValidMessageID(msgid) {
-		err = fmt.Errorf("invalid msg-id %q", line[s:i])
-		return
-	}
-	skipWS()
-	if i < len(line) {
-		err = errors.New("extra data in HDR output")
-		return
-	}
-
-	return
-}
-
 func (c *NNTPScraper) eatHdrOutput(
 	r_begin, r_end int64) (err error) {
 
@@ -712,7 +443,7 @@ func (c *NNTPScraper) eatHdrOutput(
 		var id uint64
 		var msgid FullMsgID
 
-		id, msgid, err = c.eatHdrLine(dr)
+		id, msgid, err = c.eatHdrMsgIDLine(dr)
 		if err != nil {
 			if err == io.EOF {
 				err = nil
@@ -1071,6 +802,7 @@ func (c *NNTPScraper) main() error {
 		c.log.LogPrintf(DEBUG, "LoadTempGroup(): g:%q n:%d o:%d",
 			group, new_id, old_id)
 
+		// maybe we don't need to bother with this group
 		if new_id >= 0 && old_id >= uint64(new_id) {
 			if old_id != uint64(new_id) {
 				// keep track of reduction too
@@ -1092,8 +824,19 @@ func (c *NNTPScraper) main() error {
 			// next group, I guess..
 			continue
 		}
+		// in case we had no info about new_id before, or it's higher..
 		if new_id < 0 || g_id > new_id {
 			new_id = g_id
+
+			// recheck
+			if old_id >= uint64(new_id) {
+				if old_id != uint64(new_id) {
+					// keep track of reduction too
+					c.db.UpdateGroupID(group, uint64(new_id))
+				}
+				// skip this
+				continue
+			}
 		}
 
 		e, fatal = c.eatGroup(group, old_id, uint64(new_id))
