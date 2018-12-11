1934676e834751150fd0609625ca60aa49bb7dd3
cathugger <cathugger@cock.li>
2018-12-11 00:16:49 +0000

    support more header encodings

--- src/centpd/lib/mail/mimeword.go
+++ src/centpd/lib/mail/mimeword.go
@@ -8,20 +8,28 @@ import (
 	gmail "net/mail"
 	"strings"
 	"unicode/utf8"
+
+	"golang.org/x/text/encoding/ianaindex"
 )
 
 type failCharsetError string
 
-func (f failCharsetError) Error() string {
-	return fmt.Sprintf("unhandled charset %q", string(f))
+func (e failCharsetError) Error() string {
+	return fmt.Sprintf("unhandled charset %q", string(e))
 }
 
-func failCharsetReader(charset string, input io.Reader) (io.Reader, error) {
-	// failing this way is faster than fmt.Errorf done by default
-	return nil, failCharsetError(charset)
+func insaneCharsetReader(charset string, input io.Reader) (io.Reader, error) {
+	if charset == "" {
+		return nil, failCharsetError("")
+	}
+	cod, e := ianaindex.MIME.Encoding(charset)
+	if e != nil {
+		return nil, e
+	}
+	return cod.NewDecoder().Reader(input), nil
 }
 
-var mimeWordDecoder = mime.WordDecoder{CharsetReader: failCharsetReader}
+var mimeWordDecoder = mime.WordDecoder{CharsetReader: insaneCharsetReader}
 
 // DecodeMIMEWordHeader decodes MIME header and
 // ensures result is valid UTF-8 text and does not contain null bytes
@@ -42,6 +50,8 @@ func DecodeMIMEWordHeader(s string) (_ string, err error) {
 	return s, nil
 }
 
+var addrParser = gmail.AddressParser{WordDecoder: &mimeWordDecoder}
+
 func ParseAddressX(s string) (*gmail.Address, error) {
-	return gmail.ParseAddress(s)
+	return addrParser.Parse(s)
 }
