f90bcf33028beff250dd7dc5dd08903186096ed3
cathugger <cathugger@cock.li>
2018-12-12 18:25:41 +0200

    add more tests

--- src/centpd/lib/mail/mimeword_test.go
+++ src/centpd/lib/mail/mimeword_test.go
@@ -13,6 +13,8 @@ func TestFormatAddress(t *testing.T) {
 		testcase{name: "a", email: "a@b", result: "a <a@b>"},
 		testcase{name: "a b", email: "a@b", result: "\"a b\" <a@b>"},
 		testcase{name: "a@b", email: "a@b", result: "\"a@b\" <a@b>"},
+		testcase{name: "a\"\"\"b", email: "a@b", result: "\"a\\\"\\\"\\\"b\" <a@b>"},
+		testcase{name: "a\\\\\\b", email: "a@b", result: "\"a\\\\\\\\\\\\b\" <a@b>"},
 	}
 	for i := range tests {
 		res := FormatAddress(tests[i].name, tests[i].email)
