34d156106863a65fcf02a3e2c67f70fdeedb2b2f
cathugger <cathugger@cock.li>
2019-01-15 23:52:48 +0200

    use raw utf8 instead of html entities

--- src/centpd/lib/srndtrip/srndtrip.go
+++ src/centpd/lib/srndtrip/srndtrip.go
@@ -2,14 +2,13 @@ package srndtrip
 
 import (
 	"encoding/hex"
-	"fmt"
 	"strings"
 )
 
 // this package implements unicode tripcode originally used in srnd (and srnd2)
 
 func MakeSRNdTrip(pubkey string, length int) string {
-	b := &strings.Builder{}
+	var b strings.Builder
 
 	data, err := hex.DecodeString(pubkey)
 	if err != nil {
@@ -21,8 +20,8 @@ func MakeSRNdTrip(pubkey string, length int) string {
 	}
 
 	appendch := func(ch byte) {
-		chnum := 9600 + int(ch)
-		fmt.Fprintf(b, "&#%d;", chnum)
+		chnum := 9600 + rune(ch)
+		b.WriteRune(chnum)
 	}
 	i := 0
 	for ; i < length/2; i++ {
