e355fee4b7bad25e3322c6499ae30a4491b9816c
cathugger <cathugger@cock.li>
2019-02-17 21:15:22 +0200

    use replacement in case provided extension is not ok

--- src/centpd/lib/fileutil/ext.go
+++ src/centpd/lib/fileutil/ext.go
@@ -12,6 +12,7 @@ import (
 func SafeExt(oname string) (ext string) {
 	i := strings.LastIndexByte(oname, '.')
 	if i >= 0 && i+1 < len(oname) {
+		// there is some sort of extension
 		s := oname[i+1:]
 
 		if /* 32 chars should be long enough */ len(s) <= 32 &&
@@ -20,12 +21,17 @@ func SafeExt(oname string) (ext string) {
 			// probably no one really use anything other than US-ASCII for exts
 			au.IsPrintableASCIIStr(s, 0) {
 
+			// extension should be ok
+
 			// twitter' :orig and similar shit, shouldn't change content type
 			if j := strings.IndexByte(s, ':'); j >= 0 {
 				s = s[:j]
 			}
 
 			ext = s
+		} else {
+			// provided extension is not ok, use replacement
+			ext = "bin"
 		}
 	}
 	return
