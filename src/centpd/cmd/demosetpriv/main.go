922f24c4545bc2574d1593f3b09029177b42e3c0
cathugger <cathugger@cock.li>
2019-05-25 09:08:28 +0300

    forgot actual demo

---
+++ src/centpd/cmd/demosetpriv/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"centpd/lib/democonfigs"
+	fl "centpd/lib/filelogger"
+	"centpd/lib/logx"
+	"centpd/lib/psql"
+	"centpd/lib/psqlib"
+)
+
+func main() {
+	var err error
+	// initialize flags
+	dbconnstr := flag.String("dbstr", "", "postgresql connection string")
+	modprivs := flag.String("priv", "mod", "priv to evaluate to")
+
+	flag.Parse()
+
+	// logger
+	lgr, err := fl.NewFileLogger(os.Stderr, logx.DEBUG, fl.ColorAuto)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fl.NewFileLogger error: %v\n", err)
+		os.Exit(1)
+	}
+	mlg := logx.NewLogToX(lgr, "main")
+
+	modpriv, ok := psqlib.StringToModPriv(*modprivs)
+	if !ok {
+		mlg.LogPrintln(logx.CRITICAL, "unrecognised mod priv %q", modprivs)
+		return
+	}
+
+	psqlcfg := psql.DefaultConfig
+	psqlcfg.Logger = lgr
+	psqlcfg.ConnStr = *dbconnstr
+
+	db, err := psql.OpenAndPrepare(psqlcfg)
+	if err != nil {
+		mlg.LogPrintln(logx.CRITICAL, "psql.OpenAndPrepare error:", err)
+		return
+	}
+	defer db.Close()
+
+	psqlibcfg := democonfigs.CfgPSQLIB
+	psqlibcfg.DB = &db
+	psqlibcfg.Logger = &lgr
+
+	dbib, err := psqlib.NewInitAndPrepare(psqlibcfg)
+	if err != nil {
+		mlg.LogPrintln(logx.CRITICAL, "psqlib.NewInitAndPrepare error:", err)
+		return
+	}
+
+	args := flag.Args()
+	dbib.DemoSetModPriv(args, modpriv)
+}
