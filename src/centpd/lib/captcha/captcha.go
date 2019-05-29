46cab9de94238305a534676d764f54df834a7c34
cathugger <cathugger@cock.li>
2019-05-29 12:32:44 +0300

    add some incomplete stuff

---
+++ src/centpd/lib/captcha/captcha.go
@@ -0,0 +1,11 @@
+package textcaptcha
+
+type Answer string
+type Distortion []byte
+
+/*
+ * layout for key:
+ * 8[keyid]|24[nonce]|?[encrypted data]
+ * leyout for data:
+ * 2[type]|8[expiredate]|1[x]|x[keydata]|x[random]
+ */
