75c32baa775644e3b66790b27769e68c3dca4fed
cathugger <cathugger@cock.li>
2019-05-30 03:01:40 +0300

    meow

--- src/centpd/lib/captcha/captcha.go
+++ src/centpd/lib/captcha/captcha.go
@@ -7,5 +7,5 @@ type Distortion []byte
  * layout for key:
  * 8[keyid]|24[nonce]|?[encrypted data]
  * leyout for data:
- * 2[type]|8[expiredate]|1[x]|x[keydata]|x[random]
+ * 2[type]|8[expiredate]|1[x]|x[keydata]|?[random]
  */
