ba989ce5350001a295b914f8d8d09bbc698063cd
cathugger <cathugger@cock.li>
2019-07-16 23:58:18 +0300

    actually do psqlstore, testing later

--- src/nksrv/lib/captchastore/psqlstore/psqlstore.go
+++ src/nksrv/lib/captchastore/psqlstore/psqlstore.go
@@ -1,28 +1,46 @@
 package psqlstore
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"nksrv/lib/captchastore"
+	"nksrv/lib/date"
+	. "nksrv/lib/logx"
+	"nksrv/lib/psql"
+)
+
+const currDbVersion = "0"
+
 type PSQLStore struct {
-	db *psql.PSQL
+	db  *psql.PSQL
+	log Logger
 }
 
-
-func (sp *PSQLStore) doDbInit() (err error) {
+func (p PSQLStore) doDbInit() (err error) {
 	stmts := [...]string{
 		`CREATE SCHEMA captcha`,
 		`CREATE TABLE captcha.keks (
-	kek_id INTEGER GENERATED ALWAYS AS IDENTITY,
-	kek_data BYTEA NOT NULL,
+	kek_order    BIGINT  GENERATED ALWAYS AS IDENTITY,
+	kek_id       BIGINT  NOT NULL,
+	kek_data     BYTEA   NOT NULL,
+	kek_disabled BOOLEAN NOT NULL DEFAULT FALSE,
 
-	PRIMARY KEY (kek_id)
+	PRIMARY KEY (kek_id),
+	UNIQUE (kek_order)
 )`,
 		`CREATE TABLE captcha.solved (
-	solved_key BYTEA NOT NULL,
-	solved_exp TIMESTAMP WITH TIME ZONE,
+	solved_key BYTEA                    NOT NULL,
+	solved_exp TIMESTAMP WITH TIME ZONE NOT NULL,
 
 	PRIMARY KEY (solved_key)
-)`
+)`,
+		`CREATE INDEX
+	ON captcha.solved (solved_exp)`,
 	}
 
-	tx, err := sp.db.DB.BeginTx(context.Background(), &sql.TxOptions{
+	tx, err := p.db.DB.BeginTx(context.Background(), &sql.TxOptions{
 		Isolation: sql.LevelSerializable,
 		ReadOnly:  false,
 	})
@@ -35,7 +53,7 @@ func (sp *PSQLStore) doDbInit() (err error) {
 		}
 	}()
 
-	for i, s := range stmts["init"] {
+	for i, s := range stmts {
 		_, err = tx.Exec(s)
 		if err != nil {
 			err = fmt.Errorf("err on stmt %d: %v", i, err)
@@ -50,66 +68,176 @@ func (sp *PSQLStore) doDbInit() (err error) {
 	return
 }
 
-func (sp *PSQLIB) InitIb0() {
-	e := sp.doDbIbit()
+func (p PSQLStore) InitPSQLStore() {
+	e := p.doDbInit()
 	if e != nil {
 		panic(e)
 	}
 }
 
-func (sp *PSQLIB) CheckIb0() (initialised bool, versionerror error) {
-	q := "SHOW server_version_num"
-	var vernum int64
-	err := sp.db.DB.QueryRow(q).Scan(&vernum)
-	if err != nil {
-		return false, sp.sqlError("server version query", err)
-	}
-	const verreq = 100000
-	if vernum < verreq {
-		return false, fmt.Errorf("we require at least server version %d, got %d", verreq, vernum)
-	}
-
-	q = "SELECT version FROM capabilities WHERE component = 'ib0' LIMIT 1"
+func (p PSQLStore) CheckDb() (initialised bool, err error) {
+	q := `SELECT version FROM capabilities WHERE component = 'captcha' LIMIT 1`
 	var ver string
-	err = sp.db.DB.QueryRow(q).Scan(&ver)
+	err = p.db.DB.QueryRow(q).Scan(&ver)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return false, nil
 		}
-		return false, sp.sqlError("version row query", err)
+		return false, p.sqlError("version row query", err)
 	}
 
-	if ver != currIb0Version {
-		return true, fmt.Errorf("incorrect ib0 schema version: %q (our: %q)", ver, currIb0Version)
+	if ver != currDbVersion {
+		return true, fmt.Errorf("incorrect ib0 schema version: %q (our: %q)", ver, currDbVersion)
 	}
 
 	return true, nil
 }
 
-
-
-
-func NewPSQLStore(db *psql.PSQL) captchastore.CaptchaStore {
-	s := psqlStore{db: db}
-	return s
+func NewPSQLStore(db *psql.PSQL, l LoggerX) PSQLStore {
+	log := NewLogToX(l, fmt.Sprintf("captchastore/psqlstore.%p", db))
+	return PSQLStore{db: db, log: log}
 }
 
-func (s *psqlStore) StoreSolved(
+func (p PSQLStore) StoreSolved(
 	obj []byte, expires, nowtime int64) (fresh bool, err error) {
 
+	exp_t := date.UnixTimeUTC(expires)
+	now_t := date.UnixTimeUTC(nowtime)
 
+	q := `WITH
+	x AS (
+		DELETE FROM
+			captcha.solved
+		WHERE
+			solved_exp < $3
+	)
+INSERT INTO captcha.solved (solved_key,solved_exp) VALUES ($1,$2) ON CONFLICT (solved_key) DO NOTHING RETURNING TRUE`
+	var inserted bool
+	err = p.db.DB.QueryRow(q, obj, exp_t, now_t).Scan(&inserted)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// conflict - already exists
+			return false, nil
+		}
+		return false, p.sqlError("captcha solved queryrow scan", err)
+	}
+	if !inserted {
+		// shouldn't happen
+		return false, p.sqlError("bad inserted value", err)
+	}
+	return true, nil
 }
 
-func (ms *MemStore) LoadKEKs(
+type queryer interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+}
+
+func (p PSQLStore) loadKEKs(
+	qq queryer, keks []captchastore.KEKInfo) ([]captchastore.KEKInfo, error) {
+
+	q := `SELECT kek_id,kek_data,kek_disabled FROM captcha.keks ORDER BY kek_order DESC`
+	rows, err := qq.Query(q)
+	if err != nil {
+		return keks, p.sqlError("kek query", err)
+	}
+	for rows.Next() {
+		var kek captchastore.KEKInfo
+		err = rows.Scan(&kek.ID, &kek.KEK, &kek.Disabled)
+		if err != nil {
+			return keks, p.sqlError("kek query rows scan", err)
+		}
+		keks = append(keks, kek)
+	}
+	if err = rows.Err(); err != nil {
+		return keks, p.sqlError("kek query rows iteration", err)
+	}
+	return keks, nil
+}
+
+func hasValidKEKs(keks []captchastore.KEKInfo) bool {
+	for i := range keks {
+		if !keks[i].Disabled {
+			return true
+		}
+	}
+	return false
+}
+
+func (p PSQLStore) LoadKEKs(
 	ifempty func() (id uint64, kek []byte)) (
 	keks []captchastore.KEKInfo, err error) {
 
-	ms.lock.Lock()
-	defer ms.lock.Unlock()
-
-	if len(ms.keks) == 0 && ifempty != nil {
-		id, kek := ifempty()
-		ms.keks = append(ms.keks, captchastore.KEKInfo{ID: id, KEK: kek})
+	keks, err = p.loadKEKs(p.db.DB, keks)
+	if err != nil || ifempty == nil || hasValidKEKs(keks) {
+		return
 	}
-	return ms.keks, nil
+
+	// currently no valid keks - do tx
+	tx, err := p.db.DB.Begin()
+	if err != nil {
+		return nil, p.sqlError("kek tx begin", err)
+	}
+	defer func() {
+		if err != nil {
+			_ = tx.Rollback()
+		}
+	}()
+
+	// do explicit lock to prevent concurrent access
+	_, err = tx.Exec("LOCK captcha.keks IN ACCESS EXCLUSIVE MODE")
+	if err != nil {
+		err = p.sqlError("lock exec", err)
+		return
+	}
+
+	// recheck
+	keks = keks[:0]
+	keks, err = p.loadKEKs(tx, keks)
+	if err != nil {
+		return
+	}
+	if hasValidKEKs(keks) {
+		_ = tx.Rollback() // we didn't modify anything so discard
+		return
+	}
+
+	// so we have exclusive access and there are no valid keks at this point
+	nfail := 0
+	for {
+		id, kek := ifempty()
+		var inserted bool
+		q := `INSERT INTO captcha.keks (kek_id,kek_data) VALUES ($1,$2) ON CONFLICT (kek_id) DO NOTHING RETURNING TRUE`
+		err = tx.QueryRow(q, id, kek).Scan(&inserted)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				// conflict - already exists
+				if nfail < 10 {
+					nfail++
+					continue
+				}
+			}
+			err = p.sqlError("captcha keks insert queryrow scan", err)
+			return
+		}
+		if !inserted {
+			// shouldn't happen
+			err = p.sqlError("bad inserted value", err)
+			return
+		}
+
+		// inserted OKAY
+
+		err = tx.Commit()
+		if err != nil {
+			err = p.sqlError("kek tx commit", err)
+			return
+		}
+
+		keks = append([]captchastore.KEKInfo{{ID: id, KEK: kek}}, keks...)
+		return
+	}
+}
+
+func (p PSQLStore) sqlError(when string, err error) error {
+	return psql.SQLError(p.log, when, err)
 }
