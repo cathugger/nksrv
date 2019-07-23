c2795484ca70c0427dd1aa3bc5874066a404dc4f
cathugger <cathugger@cock.li>
2019-07-23 22:13:50 +0300

    yenc idk if does shit right

---
+++ src/nksrv/lib/yenc/dec.go
@@ -0,0 +1,92 @@
+package yenc
+
+import (
+	"errors"
+	"io"
+
+	"nksrv/lib/bufreader"
+)
+
+var errUnterminatedEscape = errors.New("yenc: unterminated escape")
+
+type YDecoder struct {
+	br      *bufreader.BufReader
+	line    [1000]byte
+	linex   int
+	linelen int
+}
+
+func (d *YDecoder) fetch() (err error) {
+	var n int
+	n, err = d.br.ReadUntil(d.line[:], '\n')
+	if n > 0 && d.line[n-1] == '\n' {
+		n--
+	}
+	if n > 0 && d.line[n-1] == '\r' {
+		n--
+	}
+
+	if len(d.line) >= 2 && d.line[0] == '=' && d.line[1] == 'y' {
+		// skip any line starting =y
+		return
+	}
+
+	d.linex = 0
+	d.linelen = 0
+	for i := 0; i < n; i++ {
+		c := d.line[i]
+		if c == '=' {
+			i++
+			if i >= n {
+				if err == nil {
+					err = errUnterminatedEscape
+				}
+				break
+			}
+			c = d.line[i] - 64
+		}
+		d.line[d.linelen] = c - 42
+		d.linelen++
+	}
+
+	return
+}
+
+func (d *YDecoder) Read(p []byte) (n int, err error) {
+	for d.linex == d.linelen {
+		if err != nil {
+			return 0, err
+		}
+		err = d.fetch()
+	}
+
+	n = copy(p, d.line[d.linex:d.linelen])
+	d.linex += n
+	return
+}
+
+func (d *YDecoder) WriteTo(w io.Writer) (n int64, err error) {
+	for {
+		for d.linex == d.linelen {
+			if err != nil {
+				if err == io.EOF {
+					err = nil
+				}
+				return 0, err
+			}
+			err = d.fetch()
+		}
+
+		wn, we := w.Write(d.line[d.linex:d.linelen])
+
+		n += int64(wn)
+		d.linex += wn
+
+		if we != nil {
+			if err == nil || err == io.EOF {
+				err = we
+			}
+			return
+		}
+	}
+}
---
+++ src/nksrv/lib/yenc/enc.go
@@ -0,0 +1,68 @@
+package yenc
+
+import "io"
+
+type YEncoder struct {
+	buf  [1000]byte
+	bufi int
+	w    io.Writer
+}
+
+const lineLength = 990
+
+func (e *YEncoder) Write(b []byte) (n int, err error) {
+	for i, c := range b {
+		if e.bufi >= lineLength {
+			var nn int
+			nn, err = e.w.Write(e.buf[:e.bufi])
+			if err != nil {
+				// sorta resumable?
+				n = i
+				return
+			}
+			if nn != e.bufi {
+				panic("wrong written bytes")
+			}
+			e.bufi = 0
+		}
+
+		c += 42
+		if c == '\000' || c == '\r' || c == '\n' || c == '=' ||
+			/* pretty much all yEnc uses are inside dotwriter */
+			(e.bufi == 0 && c == '.') {
+
+			c += 64
+			e.buf[e.bufi] = '='
+			e.bufi++
+		}
+		e.buf[e.bufi] = c
+		e.bufi++
+
+		if e.bufi >= lineLength {
+			e.buf[e.bufi], e.buf[e.bufi+1] = '\r', '\n'
+			e.bufi += 2
+		}
+	}
+
+	return len(b), nil
+}
+
+func (e *YEncoder) Close() (err error) {
+	if e.bufi != 0 {
+		if e.bufi < lineLength {
+			e.buf[e.bufi], e.buf[e.bufi+1] = '\r', '\n'
+			e.bufi += 2
+		}
+		var nn int
+		nn, err = e.w.Write(e.buf[:e.bufi])
+		if err != nil {
+			e.bufi = 0
+			return
+		}
+		if nn != e.bufi {
+			panic("wrong written bytes")
+		}
+		e.bufi = 0
+	}
+	return
+}
