a420ddb1dbfa4b1b8f10a3d43be1868890e5493b
cathugger <cathugger@cock.li>
2019-07-30 03:41:27 +0300

    add javascript stuff

---
+++ _demo/demoib0/static/site.js
@@ -0,0 +1,79 @@
+// console.log("ohayo!");
+
+function onimglinkclick(e) {
+	var tgt = e.target;
+	if (tgt.className == "imgthumb") {
+		var lnk = tgt.parentElement;
+		var typ = lnk.dataset.type;
+		console.log(">image thumb clicked, type=" + typ);
+		if (typ == 'image') {
+			// atomic replace will need full copy as we gonna modify child elements
+			var newlnk = lnk.cloneNode(true);
+			newlnk.addEventListener("click", onimglinkclick);
+			newlnk.getElementsByClassName('imgthumb')[0].style.display = 'none';
+			var exps = newlnk.getElementsByClassName("imgexp");
+			if (exps.length > 0) {
+				// expansions already exist
+				exps[0].style.removeProperty('display');
+			} else {
+				// new expansion
+				var exp = document.createElement('img');
+				exp.src = lnk.href;
+				// img ones are to hint browser real size
+				// style ones are because max-width otherwise axes ratio
+				exp.width  = lnk.dataset.width;
+				exp.height = lnk.dataset.height;
+				exp.style.width = exp.style.height = "auto";
+				exp.className = 'imgexp';
+				newlnk.appendChild(exp);
+			}
+			var lp = lnk.parentElement;
+			lp.replaceChild(newlnk, lnk);
+
+			e.preventDefault();
+		}
+	} else if (tgt.className == "imgexp") {
+		// if we encounter this type then we already know that this is image
+		var lnk = tgt.parentElement;
+		var newlnk = lnk.cloneNode(true);
+		newlnk.addEventListener("click", onimglinkclick);
+		newlnk.getElementsByClassName('imgthumb')[0].style.removeProperty('display');
+		newlnk.getElementsByClassName('imgexp')[0].style.display = 'none';
+		var lp = lnk.parentElement;
+		lp.replaceChild(newlnk, lnk);
+
+		// current position from top
+		var currpos = document.documentElement.scrollTop || document.body.scrollTop;
+		// console.log("currpos: " + currpos);
+		// current element top RELATIVE TO currpos, minus some additional stuff to feel more natural
+		var filetop = newlnk.parentElement.getBoundingClientRect().top - 18;
+		
+		// if we're beyond thumbnail image
+		// NOTE: NOT whole post but just specific thumbnail
+		if (filetop < 0) {
+			// scroll to it
+			var newpos = currpos + filetop;
+			document.documentElement.scrollTop = newpos;
+			document.body.scrollTop            = newpos;
+		}
+
+		e.preventDefault();
+	}
+}
+
+function ondomload() {
+	// DOM loaded, patch document
+	console.log(">on DOM load");
+	var links = document.getElementsByClassName("imglink");
+	for (var i = 0; i < links.length; i++) {
+		links[i].addEventListener("click", onimglinkclick);
+	}
+}
+
+if (document.readyState == 'loading') {
+	// it's still loading so add listener
+	document.addEventListener('DOMContentLoaded', ondomload);
+} else {
+	// DOM already done loader so DOMContentLoaded would never come, do work now
+	ondomload();
+}
