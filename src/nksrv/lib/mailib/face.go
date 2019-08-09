469b8da892c90357bc31d96cf9a5edac0ebc7ea9
cathugger <cathugger@cock.li>
2019-08-09 04:45:33 +0300

    read Face header too, small fixup for X-Face reading

--- src/nksrv/lib/mailib/face.go
+++ src/nksrv/lib/mailib/face.go
@@ -2,8 +2,10 @@ package mailib
 
 import (
 	"bytes"
-	"image"
+	"encoding/base64"
 	"image/gif"
+	"image/png"
+	"strings"
 
 	"nksrv/lib/fstore"
 	"nksrv/lib/ftypes"
@@ -12,28 +14,73 @@ import (
 	"nksrv/lib/xface"
 )
 
+var stripSPHT = strings.NewReplacer(" ", "", "\t", "")
+
 func extractMessageFace(
 	H mail.Headers, src *fstore.FStore) (fn string, fi FileInfo, err error) {
 
-	// TODO Face header
+	// try Face header first
+	fh := H.GetFirst("Face")
+	if fh != "" {
+		fh = stripSPHT.Replace(fh)
+		pngb := make([]byte, base64.StdEncoding.DecodedLen(len(fh)))
+		n, ex := base64.StdEncoding.Decode(pngb, unsafeStrToBytes(fh))
+		if ex == nil && n > 0 {
+			pngb = pngb[:n]
+			pngr := bytes.NewReader(pngb)
+			// safety check that it can't be harmful to us or clients
+			cfg, ex := png.DecodeConfig(pngr)
+			if ex == nil && cfg.Width == 48 && cfg.Height == 48 {
+				// sanity check that image isn't corrupt
+				pngr.Reset(pngb)
+				_, ex = png.Decode(pngr)
+				if ex == nil {
 
-	xf := H.GetFirst("X-Face")
-	if xf != "" {
-		var fimg image.Image
-		fimg, err = xface.XFaceStringToImg(xf)
-		if err == nil {
+					// image in buffer suits our needs :>
+
+					var hash, hashtype string
+					var fsize int64
+					pngr.Reset(pngb)
+					fn, hash, hashtype, fsize, _, _, err =
+						takeInFile(
+							src, thumbnailer.ThumbExec{}, true,
+							"png", "image/png", pngr, true, nil)
+					if err != nil {
+						return
+					}
+
+					fi = FileInfo{
+						Type:        ftypes.FTypeFace,
+						ContentType: "image/png",
+						Size:        fsize,
+						ID:          hash + "-" + hashtype + ".png",
+						Original:    "Face",
+					}
+
+					return
+				}
+			}
+		}
+	}
+
+	// X-Face header next
+	xfh := H.GetFirst("X-Face")
+	if xfh != "" {
+		fimg, ex := xface.XFaceStringToImg(xfh)
+		if ex == nil {
 			var b bytes.Buffer
 
-			err = gif.Encode(&b, fimg, nil)
-			if err != nil {
-				panic(err)
+			ex = gif.Encode(&b, fimg, nil)
+			if ex != nil {
+				panic(ex)
 			}
 
 			var hash, hashtype string
 			var fsize int64
-			fn, hash, hashtype, fsize, _, _, err = takeInFile(
-				src, thumbnailer.ThumbExec{}, true,
-				"gif", "image/gif", &b, true, nil)
+			fn, hash, hashtype, fsize, _, _, err =
+				takeInFile(
+					src, thumbnailer.ThumbExec{}, true,
+					"gif", "image/gif", &b, true, nil)
 			if err != nil {
 				return
 			}
