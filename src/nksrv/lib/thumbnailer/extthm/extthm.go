4bd18711fafa3dad8c1ea3c95dcc735e217f4fb7
cathugger <cathugger@cock.li>
2019-08-10 07:58:17 +0300

    some probably bad sleepy code

---
+++ src/nksrv/lib/thumbnailer/extthm/extthm.go
@@ -0,0 +1,98 @@
+package extthm
+
+// thumbnailer which calls external programs and is designed to work for most of things with some amount of customization
+
+import (
+	"os"
+
+	"github.com/gobwas/glob"
+
+	"nksrv/lib/fstore"
+	"nksrv/lib/thumbnailer"
+)
+
+/*
+ * organization:
+ * mime type => {
+ *   scanner: {type, options},
+ *   thumbnailer: {type, options},
+ * }
+ */
+type Config struct {
+	MaxWidth, MaxHeight int
+	MaxPixels           int
+	MaxFileSize         int64
+}
+
+var DefaultConfig = Config{
+	MaxWidth:    8192,
+	MaxHeight:   8192,
+	MaxPixels:   6144 * 6144,
+	MaxFileSize: 64 * 1024 * 1024,
+}
+
+func (c Config) BuildThumbnailer(
+	fs *fstore.FStore) (thumbnailer.Thumbnailer, error) {
+
+	// XXX check existence of binaries
+
+	return &ExternalThumbnailer{cfg: c, fs: fs}, nil
+}
+
+type tparams = map[string]string
+
+type thmbackend interface {
+	doThumbnailing(
+		p tparams, f *os.File, ext, mimeType string, cfg thumbnailer.ThumbConfig) (
+		res thumbnailer.ThumbResult, fi thumbnailer.FileInfo, err error)
+}
+
+type ExtExec struct {
+	// matching either by mime or ext or both
+	m_mime glob.Glob
+	m_ext  glob.Glob
+	// thing actually doing execution
+	t thmbackend
+	// params to pass to thumbnailer
+	p tparams
+}
+
+type ExternalThumbnailer struct {
+	cfg    Config
+	fs     *fstore.FStore
+	routes []ExtExec
+}
+
+func (t *ExternalThumbnailer) ThumbProcess(
+	f *os.File, ext, mimeType string, cfg thumbnailer.ThumbConfig) (
+	res thumbnailer.ThumbResult, fi thumbnailer.FileInfo, err error) {
+
+	close_err := func() { err = f.Close() }
+
+	if t.cfg.MaxFileSize > 0 {
+		var st os.FileInfo
+		st, err = f.Stat()
+		if err != nil {
+			_ = f.Close()
+			return
+		}
+		if st.Size() > t.cfg.MaxFileSize {
+			close_err()
+			return
+		}
+	}
+
+	for i := range t.routes {
+		if (t.routes[i].m_mime == nil || t.routes[i].m_mime.Match(mimeType)) &&
+			(t.routes[i].m_ext == nil || t.routes[i].m_ext.Match(ext)) {
+
+			// XXX fallbacks?
+			return t.routes[i].t.
+				doThumbnailing(t.routes[i].p, f, ext, mimeType, cfg)
+		}
+	}
+
+	// matched no routes - just fug off
+	close_err()
+	return
+}
---
+++ src/nksrv/lib/thumbnailer/extthm/imagemagick.go
@@ -0,0 +1,224 @@
+package extthm
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"nksrv/lib/ftypes"
+	"nksrv/lib/thumbnailer"
+	"nksrv/lib/thumbnailer/internal/exifhelper"
+)
+
+type imagemagickBackend struct {
+	t       *ExternalThumbnailer
+	binPath string
+	useGM   bool // tbh untested
+}
+
+var errOutputMisunderstod = errors.New("convert output not understod")
+
+func (b *imagemagickBackend) doThumbnailing(
+	p tparams, f *os.File, ext, mimeType string, cfg thumbnailer.ThumbConfig) (
+	res thumbnailer.ThumbResult, fi thumbnailer.FileInfo, err error) {
+
+	closed := false
+
+	defer func() {
+		if !closed {
+			f.Close()
+		}
+	}()
+
+	close_err := func() {
+		err = f.Close()
+		closed = true
+	}
+
+	/*
+	 * how this works:
+	 * first we query params about image using golang libs
+	 * if image params are OK we pass it to imagemagick
+	 * we don't need policy files this way
+	 * and we already know what type we need to force IM/GM to use
+	 * we still need to read output for
+	 */
+
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		return
+	}
+
+	imgcfg, cfgfmt, err := image.DecodeConfig(f)
+	if err != nil {
+		// bail out on any decoder failure
+		close_err()
+		return
+	}
+	switch cfgfmt {
+	case "jpeg", "png", "gif", "webp", "bmp":
+		// OK
+	default:
+		// NAK
+		close_err()
+		return
+	}
+
+	// seek to start
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		return
+	}
+
+	// get orientation
+	orient := exifhelper.ExifOrient(f)
+
+	// rotate limits
+	imgcfg.Width, imgcfg.Height = exifhelper.RotWH(orient, imgcfg.Width, imgcfg.Height)
+
+	// mark this as image and store config
+	fi.Kind = ftypes.FTypeImage
+	fi.DetectedType = "image/" + cfgfmt
+	fi.Attrib = make(map[string]interface{})
+	fi.Attrib["width"] = imgcfg.Width
+	fi.Attrib["height"] = imgcfg.Height
+
+	if (b.t.cfg.MaxWidth > 0 && imgcfg.Width > b.t.cfg.MaxWidth) ||
+		(b.t.cfg.MaxHeight > 0 && imgcfg.Height > b.t.cfg.MaxHeight) ||
+		(b.t.cfg.MaxPixels > 0 && imgcfg.Width*imgcfg.Height > b.t.cfg.MaxPixels) {
+
+		close_err()
+
+		return
+	}
+
+	fn := f.Name()
+
+	close_err()
+	if err != nil {
+		return
+	}
+
+	// park file for convert output
+	tf, err := b.t.fs.TempFile("t-", ".jpg")
+	if err != nil {
+		return
+	}
+	tfn := tf.Name()
+	defer func() {
+		if err != nil {
+			os.Remove(tfn)
+		}
+	}()
+	err = tf.Close()
+	if err != nil {
+		return
+	}
+
+	// at this point we can call IM/GM
+
+	runfile := b.binPath
+	useGM := b.useGM
+	args := []string{runfile}
+	if useGM {
+		args = append(args, "convert")
+	}
+
+	if cfgfmt == "jpeg" {
+		// special jpeg thing
+		args = append(
+			args, "-define",
+			fmt.Sprintf("jpeg:size=%dx%d", cfg.Width, cfg.Height))
+	}
+
+	// [0] doesn't hurt
+	convsrc := cfgfmt + ":" + fn + "[0]"
+	args = append(args, convsrc)
+	// NOTE: we thumbnail disregarding profile
+	// correct would be first converting to linear RGB, but that's bad for perf
+	args = append(args, "-thumbnail", fmt.Sprintf("%dx%d", cfg.Width, cfg.Height))
+	args = append(args, "-auto-orient")
+	if cfg.Color != "" && cfgfmt != "jpeg" && cfgfmt != "bmp" {
+		// tbh this should be always set when doing JPEG thumbnails
+		args = append(args, "-background", cfg.Color, "-flatten")
+	}
+	// TODO use profile path?
+	// convert to sRGB colorspace if current is different, and strip profiles and other stuff to make smaller
+	if !useGM {
+		args = append(args, "-colorspace", "sRGB", "-strip")
+	} else {
+		// gm's sRGB is super weird one so DON'T
+		// XXX following would kill non-sRGB profiles
+		//args = append(args, "-strip")
+	}
+	// pipe to stdout
+	args = append(args, "-verbose", tfn)
+
+	cmd := &exec.Cmd{
+		Path: runfile,
+		Args: args,
+	}
+
+	var out []byte
+	var ex error
+	if !useGM {
+		out, ex = cmd.Output()
+	} else {
+		out, ex = cmd.CombinedOutput()
+	}
+	if ex != nil {
+		// XXX should log
+		// process failed prolly because file's bad but we should know for sure
+		return
+	}
+
+	// parse stderr
+	outs := string(out)
+	if i := strings.IndexByte(outs, '\n'); i >= 0 {
+		outs = outs[:i]
+	}
+	outs = strings.TrimSpace(outs)
+	details := strings.Fields(outs)
+	// fn.jpg=>tfn.jpg JPEG 1200x800=>500x333 500x333+0+0 8-bit sRGB 137060B 0.030u 0:00.013
+	// gm don't do second one and has +0+0 in first one
+	if len(details) < 3 {
+		err = errOutputMisunderstod
+		return
+	}
+	rsz := details[2]
+	if sep := strings.Index(rsz, "=>"); sep >= 0 {
+		rsz = rsz[sep+2:]
+	} else {
+		err = errOutputMisunderstod
+		return
+	}
+	if trash := strings.IndexByte(rsz, '+'); trash >= 0 {
+		rsz = rsz[:trash]
+	}
+	if eeks := strings.IndexByte(rsz, 'x'); eeks >= 0 {
+		var x uint64
+
+		x, err = strconv.ParseUint(rsz[:eeks], 10, 32)
+		if err != nil {
+			err = errOutputMisunderstod
+			return
+		}
+		res.Width = int(x)
+
+		x, err = strconv.ParseUint(rsz[eeks+1:], 10, 32)
+		if err != nil {
+			err = errOutputMisunderstod
+			return
+		}
+		res.Height = int(x)
+	}
+
+	res.FileName = tfn
+	res.FileExt = "jpg"
+
+	return
+}
