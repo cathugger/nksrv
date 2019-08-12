05fec01056b1cf1a3802c2c76e28b54c3924fb3f
cathugger <cathugger@cock.li>
2019-08-12 03:57:42 +0300

    add missing file

---
+++ src/nksrv/lib/thumbnailer/extthm/beeep.go
@@ -0,0 +1,11 @@
+package extthm
+
+func calcDecreaseThumbSize(ow, oh int, tw, th int) (int, int) {
+	rw := float64(ow) / float64(tw)
+	rh := float64(ow) / float64(th)
+	if rw >= rh {
+		return int((float64(ow) / rw) + 0.5), int((float64(oh) / rw) + 0.5)
+	} else {
+		return int((float64(ow) / rh) + 0.5), int((float64(oh) / rh) + 0.5)
+	}
+}
