28150ff1bd7034f8c24d5cbe25461c0f8b052063
cathugger <cathugger@cock.li>
2019-09-14 04:22:25 +0300

    more compat

--- buildsass.sh
+++ buildsass.sh
@@ -6,19 +6,20 @@ set -eux
 : ${sasscmd:=sassc -t expanded}
 
 tmpdir=$(mktemp -d -t nksrv-sass-XXXXXXXXXXXXXXX)
-trap "rm -rf $tmpdir" EXIT SIGTERM SIGINT
+trap "rm -rf $tmpdir" EXIT TERM INT
 
 cp -rf -t "$tmpdir" sass/* 2>/dev/null || :
 cp -rf -t "$tmpdir" usr/sass/* 2>/dev/null || :
 
-pushd "$tmpdir"
+odir=`pwd`
+cd "$tmpdir"
 
 find -type f -iregex '\./[^_].*\.scss' | while read -r file
 do
 	$sasscmd "$file" "${file%.scss}.css"
 done
 
-popd
+cd "$odir"
 
 mkdir -p $outdir
 cp -rf -t $outdir $tmpdir/*.css 2>/dev/null || :
