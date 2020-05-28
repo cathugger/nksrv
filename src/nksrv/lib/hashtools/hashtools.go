bd2aea583b14fd9ccfb7c8018ddbcbf2bf9c0504
cathugger <cathugger@cock.li>
2020-05-28 23:55:12 +0300

    stuff

--- src/nksrv/lib/hashtools/hashtools.go
+++ src/nksrv/lib/hashtools/hashtools.go
@@ -9,8 +9,7 @@ import (
 	"math/big"
 	"sync"
 
-	//"github.com/zeebo/blake3"
-
+	"github.com/zeebo/blake3"
 	"golang.org/x/crypto/blake2b"
 	"golang.org/x/sys/cpu"
 )
@@ -50,28 +49,36 @@ const (
 	ht_BLAKE2b_224        // fastest on most 64bit CPUs without dedicated crypto instructions
 	//_                     // ht_BLAKE2s_224 // faster on weak 32bit CPUs -- waiting on when x/crypto provides New224 or equivalent
 	//_                     // ht_SHA3_224 // maybe once I get some hardware to test
-	//ht_BLAKE3_224
+	ht_BLAKE3_224
 	ht_max = iota - 1
 )
 
 var h_hashes = [ht_max]fhash{
 	{newHasher: func() hash.Hash { return sha256.New224() }},
 	{newHasher: func() hash.Hash { x, _ := blake2b.New(28, nil); return x }},
-	//{newHasher: func() hash.Hash { return blake3.New() }},
+	{newHasher: func() hash.Hash { return blake3.New() }},
 }
 var h_pools [ht_max]sync.Pool
 var h_use_id byte
 var h_use_hash fhash
 var h_use_pool *sync.Pool
 
-func gethasher() hash.Hash {
-	h, _ := h_use_pool.Get().(hash.Hash)
-	if h != nil {
-		h.Reset()
+type hashstuff struct {
+	h hash.Hash
+	b *[32 * 1024]byte
+}
+
+func gethasher() *hashstuff {
+	s, _ := h_use_pool.Get().(*hashstuff)
+	if s != nil {
+		s.h.Reset()
 	} else {
-		h = h_use_hash.newHasher()
+		s = &hashstuff{
+			h: h_use_hash.newHasher(),
+			b: new([32 * 1024]byte),
+		}
 	}
-	return h
+	return s
 }
 
 func pickhash(id byte) {
@@ -111,17 +118,17 @@ func MakeFileHash(r io.Reader) (s string, e error) {
 	// first byte - hash type
 	c.b[0] = h_use_id
 	// hash
-	h := gethasher()
-	_, e = io.Copy(h, r) // XXX this loves allocating
+	hs := gethasher()
+	_, e = io.CopyBuffer(hs.h, r, hs.b[:])
 	if e != nil {
 		return
 	}
-	h.Sum(c.b[1:][:0])
-	h_use_pool.Put(h)
+	hs.h.Sum(c.b[1:][:0])
+	h_use_pool.Put(hs)
 
 	// convert to base36 number and print it
 	c.x.SetBytes(c.b[:1+hashLen])
-	xb := c.x.Append(c.b[:0], 36)
+	xb := c.x.Append(c.b[:0], 36) // this function apparently still allocates..
 
 	// flip (we want front bits to be more variable)
 	for i, j := 0, len(xb)-1; i < j; i, j = i+1, j-1 {
