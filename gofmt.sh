a6d5fb383ee485e0f9924f314ab8daf87f1be676
cathugger <cathugger@cock.li>
2020-12-17 14:41:10 +0200

    change my mind once again

--- gofmt.sh
+++ gofmt.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -ue
 
 #exec find ./ -iname '*.go' -exec gofmt -s -w '{}' ';'
 
@@ -8,7 +9,37 @@ then
 	exit 1
 fi
 
-if [ x"$1" = x'-u' ]
+UPDATE=0
+SIMPLIFY=0
+FMTALL=0
+
+while [ $# -gt 0 ]
+do
+	x="$1"
+	case "$x" in
+		-u)
+			shift
+			UPDATE=1
+			;;
+		-s)
+			shift
+			SIMPLIFY=1
+			;;
+		-a|-all)
+			shift
+			FMTALL=1
+			;;
+		-*)
+			echo "Unknown option: $x" 1>&2
+			exit 1
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
+if [ $UPDATE = 1 ]
 then
 	cd # otherwise it'll add dep to current go.mod
 	go get -u golang.org/x/tools/cmd/goimports
@@ -18,29 +49,74 @@ fi
 
 export GOPATH=`go env GOPATH`:`pwd`
 
-if [ x"$1" = x"-all" ]
-then
-	exec find \
-		-type f \
-		-name '*.go' \
-		-not -regex ".*_nofmt[._].*" \
-		-exec \
-			goimports -local 'nksrv/' -w '{}' ';'
-fi
-
-if [ x"$1" = x"-s" ]
-then
-	exec find \
+dofmtsimplify ()
+{
+	find "$1" \
 		-type f \
 		-name '*.go' \
 		-not -regex ".*_nofmt[._].*" \
 		-exec \
 			gofmt -s -w '{}' ';'
+}
+
+dofmtimports ()
+{
+	find "$1" \
+		-type f \
+		-name '*.go' \
+		-not -regex ".*_nofmt[._].*" \
+		-exec \
+			goimports -local "$2/" -w '{}' ';'
+}
+
+dofmtone ()
+{
+	printf 'formatting [%s] [%s]\n' "$1" "$2"
+
+	if [ $SIMPLIFY = 1 ]
+	then
+		dofmtsimplify "$1"
+	fi
+	dofmtimports "$1" "$2"
+}
+
+dofmt ()
+{
+	if [ "$1" = 'src' -o "$1" = 'src/' ]
+	then
+		dofmt src/*
+		return
+	fi
+
+	for x in "$@"
+	do
+		case "$x" in
+		src/*)
+			# ok
+			;;
+		*)
+			echo "Bad fmt path: $x" >&2
+			exit 1
+			;;
+		esac
+
+		m=${x#src/} # strip src prefix
+		m=${m%%/*}  # strip anything other than module name
+
+		if [ "$m" = '' ]
+		then
+			echo "Bad fmt path: $x" >&2
+			exit 1
+		fi
+
+		dofmtone "$x" "$m"
+	done
+}
+
+if [ $FMTALL = 1 -o $# -lt 1 ]
+then
+	dofmt src
+	exit
 fi
 
-exec find "$@" \
-	-type f \
-	-name '*.go' \
-	-not -regex ".*_nofmt[._].*" \
-	-exec \
-		goimports -local 'nksrv/' -w '{}' ';'
+dofmt "$@"
