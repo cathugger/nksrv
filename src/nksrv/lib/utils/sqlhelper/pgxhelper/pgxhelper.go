fb1ab0fab29d0fc3963caa7034e10b3fff1205c0
cathugger <cathugger@cock.li>
2021-03-30 21:02:48 +0300

    finish pgxhelper stuff or not really but something

--- src/nksrv/lib/utils/sqlhelper/pgxhelper/pgxhelper.go
+++ src/nksrv/lib/utils/sqlhelper/pgxhelper/pgxhelper.go
@@ -2,6 +2,7 @@ package pgxhelper
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/fs"
 	"reflect"
@@ -14,32 +15,16 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
-func DoStuffConfig(cfg *pgx.ConnConfig, comp string, dir fs.FS) (err error) {
-	conn, err := pgx.ConnectConfig(context.Background(), cfg)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		e := conn.Close(context.Background())
-		if err == nil && e != nil {
-			err = e
-		}
-	}()
-
-	err = DoStuffConn(conn, comp, dir)
-	return
-}
-
 type schemaAndVer struct {
 	s []string
 	v int
 }
 
 type PGXSchemaTool struct {
-	current    []string // "current" seed
+	current    []string       // "current" seed
 	seeds      []schemaAndVer // various versions seeds
 	migrations []schemaAndVer // version upgrades
-	maxVer     int // max ver, either ver of "current" or maximum reachable via seeds and migrations
+	maxVer     int            // max ver, either ver of "current" or maximum reachable via seeds and migrations
 }
 
 func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
@@ -49,9 +34,9 @@ func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
 	var objects = make(map[string][]string)
 	var currentVer = -1
 
-	e := fs.WalkDir(dir, "schema", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
+	err = fs.WalkDir(dir, "schema", func(path string, d fs.DirEntry, xerr error) error {
+		if xerr != nil {
+			return xerr
 		}
 		if d.IsDir() {
 			if d.Name()[0] == '.' || d.Name()[0] == '_' {
@@ -66,13 +51,13 @@ func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
 			return fmt.Errorf("%q is not sql file", path)
 		}
 
-		q, err := sqlbucket.New().
+		q, xerr := sqlbucket.New().
 			WithName("x").
 			WithNeedSemicolon(true).
 			WithNoNext(true).
 			LoadFromFS(dir, path)
-		if err != nil {
-			return err
+		if xerr != nil {
+			return xerr
 		}
 
 		ver := strings.TrimPrefix(path, "schema/")
@@ -89,8 +74,8 @@ func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
 				return fmt.Errorf("current version redeclared")
 			}
 			sv := q["version"][0]
-			cv, err := strconv.ParseUint(sv, 10, 32)
-			if err != nil || cv > 0x7FffFFff {
+			cv, e := strconv.ParseUint(sv, 10, 32)
+			if e != nil || cv > 0x7FffFFff {
 				return fmt.Errorf("invalid current version %q", sv)
 			}
 			currentVer = int(cv)
@@ -98,10 +83,9 @@ func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
 
 		return nil
 	})
-	if e != nil {
-		return e
+	if err != nil {
+		return
 	}
-
 
 	for k, v := range objects {
 		if k == "current" {
@@ -110,9 +94,10 @@ func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
 		}
 		if k[0] == 'v' {
 			sv := k[1:]
-			cv, err := strconv.ParseUint(sv, 10, 32)
-			if err != nil || cv > 0x7FffFFff {
-				return fmt.Errorf("invalid v version %q", sv)
+			cv, e := strconv.ParseUint(sv, 10, 32)
+			if e != nil || cv > 0x7FffFFff {
+				err = fmt.Errorf("invalid v version %q", sv)
+				return
 			}
 			tool.migrations = append(tool.migrations, schemaAndVer{
 				s: v,
@@ -122,9 +107,10 @@ func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
 		}
 		if k[0] == 's' {
 			sv := k[1:]
-			cv, err := strconv.ParseUint(sv, 10, 32)
-			if err != nil || cv > 0x7FffFFff {
-				return fmt.Errorf("invalid s version %q", sv)
+			cv, e := strconv.ParseUint(sv, 10, 32)
+			if e != nil || cv > 0x7FffFFff {
+				err = fmt.Errorf("invalid s version %q", sv)
+				return
 			}
 			tool.seeds = append(tool.seeds, schemaAndVer{
 				s: v,
@@ -132,7 +118,8 @@ func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
 			})
 			continue
 		}
-		return fmt.Errorf("unknown item %q", k)
+		err = fmt.Errorf("unknown item %q", k)
+		return
 	}
 
 	sort.Slice(tool.migrations, func(i, j int) bool {
@@ -143,119 +130,172 @@ func NewSchemaTool(dir fs.FS) (_ PGXSchemaTool, err error) {
 	})
 	for i := 1; i < len(tool.seeds); i++ {
 		if tool.seeds[i-1].v == tool.seeds[i].v {
-			return fmt.Errorf(
+			err = fmt.Errorf(
 				"invalid config: duplicate seed entry")
+			return
 		}
 	}
 	for i := 1; i < len(tool.migrations); i++ {
 		if tool.migrations[i-1].v == tool.migrations[i].v {
-			return fmt.Errorf(
+			err = fmt.Errorf(
 				"invalid config: duplicate migration entry")
+			return
 		}
 	}
 
 	if tool.current != nil {
 		ms := tool.seeds[len(tool.seeds)-1]
 		if ms.v > currentVer {
-			return fmt.Errorf(
+			err = fmt.Errorf(
 				"invalid config: current ver %d < seed ver %d", currentVer, ms.v)
+			return
 		}
 		if ms.v == currentVer && !reflect.DeepEqual(ms.s, tool.current) {
-			return fmt.Errorf(
+			err = fmt.Errorf(
 				"invalid config: current and seed mismatch")
+			return
 		}
-		if mmv := migrations[len(migrations)-1].v; mmv > currentVer {
-			return fmt.Errorf(
+		if mmv := tool.migrations[len(tool.migrations)-1].v; mmv > currentVer {
+			err = fmt.Errorf(
 				"invalid config: current ver %d < migration ver %d", currentVer, mmv)
+			return
 		}
 	}
 
 	tool.maxVer = currentVer
-	if msv := seeds[len(seeds)-1].v; msv > tool.maxVer {
+	if msv := tool.seeds[len(tool.seeds)-1].v; msv > tool.maxVer {
 		tool.maxVer = msv
 	}
-	if mmv := migrations[len(migrations)-1].v; mmv > tool.maxVer {
+	if mmv := tool.migrations[len(tool.migrations)-1].v; mmv > tool.maxVer {
 		tool.maxVer = mmv
 	}
 
 	return tool, nil
 }
 
-var ErrNeedsUpgrade = errors.New("database needs update")
+func (tool *PGXSchemaTool) CheckDBConfig(cfg *pgx.ConnConfig, comp string) (err error) {
+	conn, err := pgx.ConnectConfig(context.Background(), cfg)
+	if err != nil {
+		return
+	}
+	defer func() {
+		e := conn.Close(context.Background())
+		if err == nil && e != nil {
+			err = e
+		}
+	}()
 
-func (tool *PGXSchemaTool) CheckDBVersionConn(conn *pgx.Conn, comp string) error {
+	err = tool.CheckDBConn(conn, comp)
+	return
+}
+
+func (tool *PGXSchemaTool) MigrateDBConfig(cfg *pgx.ConnConfig, comp string) (didSomething bool, err error) {
+	conn, err := pgx.ConnectConfig(context.Background(), cfg)
+	if err != nil {
+		return
+	}
+	defer func() {
+		e := conn.Close(context.Background())
+		if err == nil && e != nil {
+			err = e
+		}
+	}()
+
+	didSomething, err = tool.MigrateDBConn(conn, comp)
+	return
+}
+
+var ErrNeedsMigrate = errors.New("database needs update")
+
+func (tool *PGXSchemaTool) CheckDBConn(conn *pgx.Conn, comp string) error {
 	nowVer, err := getVersion(conn, `SELECT version FROM public.capabilities WHERE component = $1`, comp)
 	if err != nil {
 		return err
 	}
-	if nowVer > tool.maxVer {
-		return fmt.Errorf("database version higher than our (db: %d, our: %d)", nowVer, tool.maxVer)
-	}
 	if nowVer == tool.maxVer {
+		// we don't need to migrate
 		return nil
+	}
+	if nowVer < 0 {
+		// needs initialization
+		return ErrNeedsMigrate
+	}
+	err = tool.checkCanUpgrade(nowVer)
+	if err != nil {
+		return err
+	}
+	// we could do it
+	return ErrNeedsMigrate
+}
+
+var errVersionRace = errors.New("version race")
+
+func (tool *PGXSchemaTool) checkCanUpgrade(nowVer int) error {
+	if nowVer > tool.maxVer {
+		return fmt.Errorf("database version higher than our (db: v%d, our: v%d)", nowVer, tool.maxVer)
 	}
 	for _, m := range tool.migrations {
 		if nowVer+1 < m.v {
 			// migration version too low
 			continue
 		}
-		// migration version seems either equal or higher
 		nowVer++
+		// migration version seems either equal or higher
 		if nowVer > m.v {
-			return fmt.Errorf("database needs update to %d version, but we can't perform it", nowVer)
+			return fmt.Errorf("database needs update to v%d, but we can't perform it", nowVer)
 		}
-		// nowVer == m.v
 	}
 	if nowVer != tool.maxVer {
-		return fmt.Errorf("database needs update to %d version, but we can't perform it", nowVer)
+		return fmt.Errorf("database needs update to v%d from v%d, but we can't perform it", tool.maxVer, nowVer)
 	}
-	// we could do it
-	return ErrNeedsUpgrade
+	return nil
 }
 
-var errVersionRace = errors.New("version race")
+func (tool *PGXSchemaTool) checkCanSeed() error {
+	if tool.current != nil {
+		return nil
+	}
+	if len(tool.seeds) == 0 {
+		return fmt.Errorf("cannot initialize database (no seeds)")
+	}
+	return tool.checkCanUpgrade(tool.seeds[len(tool.seeds)-1].v)
+}
 
-func (tool *PGXSchemaTool) UpgradeDBVersionConn(conn *pgx.Conn, comp string) (didSomething bool, err error) {
+func (tool *PGXSchemaTool) MigrateDBConn(conn *pgx.Conn, comp string) (didSomething bool, err error) {
+
+	cRepeat := 0
 
 reVer:
-
 	nowVer, err := getVersion(conn, `SELECT version FROM public.capabilities WHERE component = $1`, comp)
 	if err != nil {
 		return
 	}
-	c := 1
-	if nowVer < 0 {
-		if tool.current != nil {
-			c = tool.maxVer-nowVer
-		}
-	}
-	// first check whether we actually can perform upgrade
-	if nowVer > tool.maxVer {
-		err = fmt.Errorf("database version higher than our (db: %d, our: %d)", nowVer, tool.maxVer)
-	}
+
 	if nowVer == tool.maxVer {
-		return nil
-	}
-	for _, m := range tool.migrations {
-		if nowVer+c < m.v {
-			// migration version too low
-			continue
-		}
-		// migration version seems either equal or higher
-		if nowVer+c > m.v {
-			err = fmt.Errorf("database needs update to %d version, but we can't perform it", nowVer+c)
-		}
-		// nowVer+c == m.v
-		c++
-	}
-	if nowVer+c != tool.maxVer {
-		err = fmt.Errorf("database needs update to %d version, but we can't perform it", nowVer)
+		// we don't actually need update
 		return
 	}
 
-	err = tool.performUpgrade(conn, comp, nowVer)
+	if nowVer >= 0 {
+		err = tool.checkCanUpgrade(nowVer)
+		if err != nil {
+			return
+		}
+		err = tool.performUpgrade(conn, comp, nowVer)
+	} else {
+		err = tool.checkCanSeed()
+		if err != nil {
+			return
+		}
+		err = tool.performSeed(conn, comp, nowVer)
+	}
 	if err != nil {
 		if err == errVersionRace {
+			if cRepeat >= 10 {
+				return
+			}
+			cRepeat++
+
 			goto reVer
 		}
 		return
@@ -268,13 +308,13 @@ reVer:
 func (tool *PGXSchemaTool) performUpgrade(conn *pgx.Conn, comp string, nowVer int) (err error) {
 
 	tx, err := conn.BeginTx(context.Background(), pgx.TxOptions{
-		IsoLevel: pgx.RepeatableRead,
+		IsoLevel:   pgx.RepeatableRead,
 		AccessMode: pgx.ReadWrite,
 	})
 	if err != nil {
 		return err
 	}
-	defer func(){
+	defer func() {
 		if err != nil {
 			tx.Rollback(context.Background())
 		}
@@ -310,6 +350,66 @@ func (tool *PGXSchemaTool) performUpgrade(conn *pgx.Conn, comp string, nowVer in
 	return
 }
 
+func (tool *PGXSchemaTool) performSeed(conn *pgx.Conn, comp string, nowVer int) (err error) {
+
+	tx, err := conn.BeginTx(context.Background(), pgx.TxOptions{
+		IsoLevel:   pgx.RepeatableRead,
+		AccessMode: pgx.ReadWrite,
+	})
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback(context.Background())
+		}
+	}()
+
+	nowVer2, err := getVersion(tx, `SELECT version FROM public.capabilities WHERE component = $1 FOR UPDATE`, comp)
+	if err != nil {
+		return err
+	}
+	if nowVer != nowVer2 {
+		err = errVersionRace
+		return
+	}
+
+	_, err = tx.Exec(context.Background(), `UPDATE public.capabilities SET version=$2 WHERE component = $1`, comp, tool.maxVer)
+	if err != nil {
+		return
+	}
+
+	if tool.current != nil {
+		err = executeMigration(tx, tool.current)
+		if err != nil {
+			return
+		}
+	} else {
+		s := tool.seeds[len(tool.seeds)-1]
+
+		err = executeMigration(tx, s.s)
+		if err != nil {
+			return
+		}
+		nowVer = s.v
+
+		for _, m := range tool.migrations {
+			if nowVer+1 < m.v {
+				// migration version too low
+				continue
+			}
+			err = executeMigration(tx, m.s)
+			if err != nil {
+				return
+			}
+			nowVer++
+		}
+	}
+
+	err = tx.Commit(context.Background())
+	return
+}
+
 func executeMigration(tx pgx.Tx, s []string) error {
 	for _, v := range s {
 		_, err := tx.Exec(context.Background(), v, pgx.QuerySimpleProtocol(true))
@@ -327,7 +427,6 @@ type pgxQueryer interface {
 
 func getVersion(q pgxQueryer, stmt, comp string) (_ int, err error) {
 	var ver int
-	stmt :=
 	err = q.QueryRow(context.Background(), stmt, pgx.QuerySimpleProtocol(true), comp).Scan(&ver)
 	if err != nil {
 		if err == pgx.ErrNoRows {
